020d001e1287c5a4f8e974640a20f8fbf042a080
Sudhanva <nsudhanva@gmail.com>
2018-08-29 20:29:45 +0530

    Removed old files 2

--- IWT/Color/HTML Color Codes Chart_files/feedback.js
+++
@@ -1,9 +0,0 @@
-function OnSubFb()
-{
-	ref = document.feedback;
-	ref.URL.value = document.URL;
-	if( ref.Message.value=="" )
-		alert('Please enter your message');
-	else
-		ref.submit();
-}
--- IWT/javascript/all/random.js
+++
@@ -1,5 +0,0 @@
-var random = document.getElementById("random");
-
-for(i = 0; i <= 100; i++){
-	random.innerHTML = random.innerHTML + Math.floor((Math.random() * 100) + 1) + "<br>";
-}
