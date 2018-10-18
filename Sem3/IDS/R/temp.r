eb608ee7ca86b1fded00c02056d2c35aa17bb668
Sudhanva Narayana <nsudhanva@gmail.com>
2018-10-18 20:43:46 +0530

    R Assignment part 1

---
+++ Sem3/IDS/R/temp.r
@@ -0,0 +1,2 @@
+cor(bull_riders$CupPoints12, bull_riders$Earnings12) 
+cor(bull_riders$RidePer12, bull_riders$Earnings12)
--- Sem3/IDS/assignment5/bull_riders.r
+++ Sem3/IDS/assignment5/bull_riders.r
@@ -1,2 +1,19 @@
 bull_riders = read.csv('BullRiders.csv')
 
+cor(bull_riders$CupPoints12, bull_riders$Earnings12)
+plot(bull_riders$CupPoints12, bull_riders$Earnings12)
+abline(lm(bull_riders$Earnings12~bull_riders$CupPoints12), col="red")
+
+bull_riders$Earnings12[bull_riders$Earnings12 < max(bull_riders$Earnings12)]
+
+cor(bull_riders$RidePer12, bull_riders$Earnings12)
+plot(bull_riders$RidePer12, bull_riders$Earnings12)
+abline(lm(bull_riders$Earnings12~bull_riders$RidePer12), col="red")
+
+cor(bull_riders$CupPoints13, bull_riders$Earnings13)
+plot(bull_riders$CupPoints13, bull_riders$Earnings13)
+abline(lm(bull_riders$Earnings13~bull_riders$CupPoints13), col="red")
+
+cor(bull_riders$RidePer14, bull_riders$Earnings14)
+plot(bull_riders$CupPoints14, bull_riders$Earnings14)
+abline(lm(bull_riders$Earnings14~bull_riders$CupPoints14), col="red")
