e088e531f862d471104b275c1e5f17735deee68c
Sudhanva Narayana <nsudhanva@gmail.com>
2018-10-23 19:06:08 +0530

    Ada notes

--- Sem3/IDS/assignment5/bull_riders.r
+++ Sem3/IDS/assignment5/bull_riders.r
@@ -4,7 +4,10 @@ cor(bull_riders$CupPoints12, bull_riders$Earnings12)
 plot(bull_riders$CupPoints12, bull_riders$Earnings12)
 abline(lm(bull_riders$Earnings12~bull_riders$CupPoints12), col="red")
 
-bull_riders$Earnings12[bull_riders$Earnings12 < max(bull_riders$Earnings12)]
+max_value = max(bull_riders_12$Earnings12)
+bull_riders_12_no_outliers =  bull_riders[!(bull_riders$Earnings12 == max_value),]
+cor(bull_riders_12_no_outliers$CupPoints12, bull_riders_12_no_outliers$Earnings12)
+cor(bull_riders_12_no_outliers$RidePer12, bull_riders_12_no_outliers$Earnings12)
 
 cor(bull_riders$RidePer12, bull_riders$Earnings12)
 plot(bull_riders$RidePer12, bull_riders$Earnings12)
