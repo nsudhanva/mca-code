dc481beb9b74f2ec1effc79d8d10d7fd846caca6
Sudhanva Narayana <nsudhanva@gmail.com>
2018-11-25 15:49:38 +0530

    Python assignment 9 gui

--- Sem3/Python/assignment9/numpy/numpy_arrays.py
+++ Sem3/Python/assignment9/numpy/numpy_arrays.py
@@ -8,7 +8,7 @@ print(type(arr))
 print(arr.dtype)
 print(arr.ndim)
 
-# 2. Create an array from a regular Python list of tuple
+# 2. Create an array from a regular python list or tuple
 arr = np.array([[1, 2, 3], [4, 5, 6]])
 print(arr)
 
