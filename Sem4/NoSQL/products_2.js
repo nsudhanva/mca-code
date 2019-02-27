1773e57d9bbc31f1a375ef70de9131d943126147
Sudhanva Narayana <nsudhanva@gmail.com>
2019-02-27 10:07:09 +0530

    Mongo aggregate, project, switch 2

--- Sem4/NoSQL/products_2.js
+++ Sem4/NoSQL/products_2.js
@@ -17,3 +17,25 @@ db.products.aggregate(
         }
     ]
 )
+
+// create an output collection of the sales data where you have name, id and total amount where price * qty and name it as sales details
+// from this output collection if the amount is greater than 1000, too costly, (amount is between 750 and 1000) should be reduced
+// 500 and 750 can be reduced
+
+db.students.aggregate([{
+    $project:
+    {
+        name: 1,
+        summary:
+        {
+            $switch:
+            {
+                branches: [
+                    { case: { $gte: [{ $avg: "$marks" }, 60] }, then: "good" },
+                    { case: { $lt: [{ $avg: "$marks" }, 45] }, then: "can do better" }
+                ],
+                default: "no scores found"
+            }
+        }
+    }
+}])
