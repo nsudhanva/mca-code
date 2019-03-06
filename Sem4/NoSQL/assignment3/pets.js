b7bc24ae40c7de2b2fc6c6ce9ddbe939333cd8a6
Sudhanva Narayana <nsudhanva@gmail.com>
2019-03-06 10:15:19 +0530

    NoSql assignment 3

--- Sem4/NoSQL/assignment3/pets.js
+++ Sem4/NoSQL/assignment3/pets.js
@@ -1,5 +1,7 @@
 db.createCollection('pets');
 
+// { "ok" : 1 }
+
 db.pets.insert(
     [
         {
@@ -134,6 +136,45 @@ db.pets.insert(
     ]
 )
 
-db.pets.aggregate(
+/*
+BulkWriteResult({
+        "writeErrors" : [ ],
+        "writeConcernErrors" : [ ],
+        "nInserted" : 9,
+        "nUpserted" : 0,
+        "nMatched" : 0,
+        "nModified" : 0,
+        "nRemoved" : 0,
+        "upserted" : [ ]
+})
+*/
 
-)
+db.pets.find({ "pets.pettype": "Cat" }).count()
+
+// 8
+
+db.pets.find({ pets: { $exists: true }, $where: 'this.pets.length > 2' })
+
+/*
+ { "_id" : ObjectId("5c7f4fcfc65811c969ae9843"), "name" : "Daya", "age" : 24, "pets" : [ { "petname" : "GG", "petage" : 1, "pettype" : "Dog" },
+{ "petname" : "Shreedhar", "petage" : 2, "pettype" : "Cat" }, { "petname" : "Yolo", "petage" : 3, "pettype" : "Cat" } ] }
+*/
+
+db.pets.find({ "pets.pettype": "Dog" }).limit(2)
+
+/*
+{ "_id" : ObjectId("5c7f4fcfc65811c969ae983d"), "name" : "Sudhanva", "age" : 24, "pets" : [ { "petname" : "Prateek", "petage" : 5, "pettype" :
+"Dog" }, { "petname" : "Shubam", "petage" : 2, "pettype" : "Cat" } ] }
+{ "_id" : ObjectId("5c7f4fcfc65811c969ae9840"), "name" : "Kiran", "age" : 20, "pets" : [ { "petname" : "Chandra", "petage" : 6, "pettype" : "Dog" }, { "petname" : "Shreedhar", "petage" : 2, "pettype" : "Cat" } ] }
+*/
+
+db.pets.find({ "pets.pettype": "Dog" }, { _id: 1, 'pets.$': 1 }) 
+
+/*
+{ "_id" : ObjectId("5c7f4fcfc65811c969ae983d"), "pets" : [ { "petname" : "Prateek", "petage" : 5, "pettype" : "Dog" } ] }
+{ "_id" : ObjectId("5c7f4fcfc65811c969ae9840"), "pets" : [ { "petname" : "Chandra", "petage" : 6, "pettype" : "Dog" } ] }
+{ "_id" : ObjectId("5c7f4fcfc65811c969ae9841"), "pets" : [ { "petname" : "Rakesh", "petage" : 6, "pettype" : "Dog" } ] }
+{ "_id" : ObjectId("5c7f4fcfc65811c969ae9842"), "pets" : [ { "petname" : "Tom", "petage" : 6, "pettype" : "Dog" } ] }
+{ "_id" : ObjectId("5c7f4fcfc65811c969ae9843"), "pets" : [ { "petname" : "GG", "petage" : 1, "pettype" : "Dog" } ] }
+{ "_id" : ObjectId("5c7f4fcfc65811c969ae9845"), "pets" : [ { "petname" : "Snoop", "petage" : 6, "pettype" : "Dog" } ] }
+*/
